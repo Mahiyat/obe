bfddb2a1300dc044ef0b75805a0aef939b38d46f
Mahiyat <mahiyat.tanzim@gmail.com>
2023-12-25 17:21:53 +0600

    Add APIs

--- client/src/components/ResultDashboard.jsx
+++ client/src/components/ResultDashboard.jsx
@@ -1,84 +1,236 @@
-import { Box, Typography } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Box, Button, Typography } from "@mui/material";
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridRowEditStopReasons,
+  GridRowModes,
+  GridToolbarContainer,
+} from "@mui/x-data-grid";
+import { randomId } from "@mui/x-data-grid-generator";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Close";
 
 import ResultDashboardRowActions from "./ResultDashboardRowActions";
-import { useEffect, useState } from "react";
+import { API_URL } from "../constants";
 
-const columns = [
-  {
-    field: "id",
-    headerName: "Sl No",
-    width: 350,
-  },
-  { field: "course_id", headerName: "Course ID", width: 90 },
-  {
-    field: "course_name",
-    headerName: "Course Name",
-    width: 350,
-  },
-  {
-    field: "exam_title",
-    headerName: "Exam Title",
-    width: 350,
-  },
-  {
-    field: "actions",
-    headerName: "Details",
-    width: 200,
-    renderCell: (params) => (
-      <ResultDashboardRowActions
-        courseId={params.value.courseId}
-        courseName={params.value.courseName}
-        title={params.value.title}
-      />
-    ),
-  },
-];
+function EditToolbar(props) {
+  const { setCourses, setRowModesModel } = props;
 
-// const rows = [
-//   {
-//     id: 1,
-//     course_id: "CSE-105",
-//     course_name: "Structured Programming Language",
-//     exam_title: "1st Year 1st Semester B.Sc. 2021",
-//     actions: {
-//       courseId: "CSE-105",
-//       courseName: "Structured Programming Language",
-//       title: "1st Year 1st Semester B.Sc. 2021",
-//     },
-//   },
-//   {
-//     id: 2,
-//     course_id: "CSE-107",
-//     course_name: "Electrical Circuit",
-//     exam_title: "1st Year 1st Semester B.Sc. 2021",
-//     actions: {
-//       courseId: "CSE-107",
-//       courseName: "Electrical Circuit",
-//       title: "1st Year 1st Semester B.Sc. 2021",
-//     },
-//   },
-// ];
+  const handleClick = async () => {
+    try {
+      const response = await axios.post(API_URL, {
+        course_id: " ",
+        course_name: " ",
+        exam_title: " ",
+      });
+      const newRecord = response.data;
+      const id = newRecord.id;
+      setCourses((oldRows) => [
+        ...oldRows,
+        {
+          id,
+          course_id: '',
+          course_name: '',
+          exam_title: '',
+          details: {
+            courseId: '',
+            courseName: '',
+            title: '',
+          },
+          isNew: true,
+        },
+      ]);
+      setRowModesModel((oldModel) => ({
+        ...oldModel,
+        [id]: { mode: GridRowModes.Edit, fieldToFocus: "course_id" },
+      }));
+    } catch (error) {
+      console.error("Error adding record:", error);
+    }
+  };
+
+  return (
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Add course
+      </Button>
+    </GridToolbarContainer>
+  );
+}
 
 export default function ResultDashboard() {
   const [courses, setCourses] = useState([]);
+  const [rowModesModel, setRowModesModel] = useState({});
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/courses/")
-      .then((data) => data.json())
-      .then((data) => setCourses(data));
+    fetchData();
   }, []);
-  // console.log(rows);
+
+  const fetchData = () => {
+    axios
+      .get(API_URL)
+      .then((response) => {
+        setCourses(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
 
   const getFormattedRows = () =>
     courses.map((course) => ({
-      ...course.fields,
-      id: course.pk,
-      actions: {
-        courseId: course.fields.coure_id,
-        courseName: course.fields.course_name,
-        title: course.fields.exam_title,
+      ...course,
+      details: {
+        courseId: course.course_id,
+        courseName: course.course_name,
+        title: course.exam_title,
       },
     }));
+
+  const handleRowEditStop = (params, event) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id) => () => {
+    processRowDelete(id);
+    setCourses(courses.filter((row) => row.id !== id));
+  };
+
+  const handleCancelClick = (id) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = courses.find((row) => row.id === id);
+    if (editedRow.isNew) {
+      setCourses(courses.filter((row) => row.id !== id));
+    }
+  };
+
+  const processRowUpdate = async (newRow) => {
+    const api = API_URL + `${newRow.id}`;
+    try {
+      await axios.put(api, newRow);
+      const updatedRowFromServer = newRow;
+      setCourses((prevCourses) =>
+        prevCourses.map((row) =>
+          row.id === updatedRowFromServer.id ? updatedRowFromServer : row
+        )
+      );
+      return updatedRowFromServer;
+    } catch (error) {
+      console.error("Error updating data:", error);
+      throw error;
+    }
+  };
+
+  const processRowDelete = async (id) => {
+    const api = API_URL + `${id}`;
+    try {
+      await axios.delete(api);
+    } catch (error) {
+      console.error("Error deleting data:", error);
+      throw error;
+    }
+  };
+
+  const handleRowModesModelChange = (newRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const columns = [
+    // {
+    //   field: "id",
+    //   headerName: "Sl No",
+    //   width: 350,
+    // },
+    { field: "course_id", headerName: "Course ID", width: 90, editable: true },
+    {
+      field: "course_name",
+      headerName: "Course Name",
+      width: 350,
+      editable: true,
+    },
+    {
+      field: "exam_title",
+      headerName: "Exam Title",
+      width: 350,
+      editable: true,
+    },
+    {
+      field: "details",
+      headerName: "Details",
+      width: 200,
+      renderCell: (params) => (
+        <ResultDashboardRowActions
+          courseId={params.value.courseId}
+          courseName={params.value.courseName}
+          title={params.value.title}
+        />
+      ),
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: "primary.main",
+              }}
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
 
   return (
     <Box sx={{ height: 400, display: "flex", flexGrow: "1" }}>
@@ -96,6 +248,11 @@ export default function ResultDashboard() {
         <DataGrid
           rows={getFormattedRows()}
           columns={columns}
+          editMode="row"
+          rowModesModel={rowModesModel}
+          onRowModesModelChange={handleRowModesModelChange}
+          onRowEditStop={handleRowEditStop}
+          processRowUpdate={processRowUpdate}
           initialState={{
             pagination: {
               paginationModel: {
@@ -106,6 +263,12 @@ export default function ResultDashboard() {
           pageSizeOptions={[5]}
           checkboxSelection
           disableRowSelectionOnClick
+          slots={{
+            toolbar: EditToolbar,
+          }}
+          slotProps={{
+            toolbar: { setCourses, setRowModesModel },
+          }}
         />
       </Box>
     </Box>
--- server/obeserver/courses/models.py
+++ server/obeserver/courses/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-  course_id=models.CharField(max_length=10)
-  course_name=models.CharField(max_length=40)
-  exam_title=models.CharField(max_length=60)
+  course_id=models.CharField(max_length=10, blank=True, null=True)
+  course_name=models.CharField(max_length=40, blank=True, null=True)
+  exam_title=models.CharField(max_length=60, blank=True, null=True)
   def __str__(self) -> str:
     return self.course_id + ": "+self.exam_title
